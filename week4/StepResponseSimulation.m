95d1396d60cd6bbc80f7c2a5c5d896f930f9bdd6
WuTuT <762641171@qq.com>
2018-06-14 20:33:17 +0800

    不知道以前误以为写对了多少代码...

--- week4/StepResponseSimulation.m
+++ week4/StepResponseSimulation.m
@@ -1,15 +1,14 @@
-clear
-s=tf('s');h=0.01;td=100;
+s=tf('s');h=0.01;td=10;
 g=[1/(0.4*s+1);10/(2*s+1)];
 % g=[1/(10*s+1);1/(20*s+1)];
 
-% sys1=c2d(1/(0.4*s+1),h,'ZOH');
-% sys2=c2d(10/(2*s+1),h,'ZOH');
+sys1=c2d(1/(0.4*s+1),h,'ZOH');
+sys2=c2d(10/(2*s+1),h,'ZOH');
 % % sys1=c2d(1/(10*s+1),h,'ZOH');
 % % sys2=c2d(1/(20*s+1),h,'ZOH');
 % sysd=feedback(sys1*sys2,1);
 % figure(1)
-% step(sysd,0:0.01:100);
+% yreal1=step(sysd,0:0.01:10);
 
 F=[1;0];
 P=[0 -1;
@@ -20,9 +19,18 @@ tn=fix(td/h)+1;
 M=ones(1,tn);
 linkn=size(g,1);
 link =cell(linkn,4);
-link(1,:) = {'zhihuan',0.1,[],1};
+link(1,:) = {'zhihuan',1,[],0.1};
 link(2,:) = {'none',[],[],[]};
 [t,y]=tfsimulation(g,F,M,P,C,h,td,link);
 hold on
-%plot(t,y,'--r')
-plot(t,y)
+plot(t,y,'r')
+y1=simout1.data;
+hold on
+plot(t,y1,'b--')
+error1=sum(abs(y1'-y))/tn
+figure(2)
+y2=simout2.data;
+plot(t,y,'r')
+hold on
+plot(t,y2,'b--')
+error2=sum(abs(y2'-y))/tn
--- week4/tfsimulation.m
+++ week4/tfsimulation.m
@@ -7,9 +7,11 @@ gd=c2d(g,h,'ZOH');
 [num, den]=tfdata(gd,'v');
 if iscell(num)
     num=cell2mat(num);
+    %double(num)
 end
 if iscell(den)
     den=cell2mat(den);
+    %double(den)
 end
 u=zeros(size(num));
 x=zeros(size(den,1),size(den,2)-1);
@@ -23,10 +25,11 @@ for i=2:tn
     u1=F*M(:,i)+P*x;
     u(:,1:end-1)=u(:,2:end);
     u(:,end)=nonlinearlink(u1,link);
-    y(i)=C*x;
+    
     for j=1:size(x,1)
         x1(j)=-den(j,2:end)*x(j,:)'+num(j,:)*u(j,:)';
     end
+    y(i)=C*x1;
     x(:,1:end-1)=x(:,2:end);
     x(:,end)=x1;
 end
